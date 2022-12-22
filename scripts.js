b2e8a8db78f638a93745a8318c805fd6bad9efa7
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-22 18:20:12 +0530

    Adding Necessary Script

---
+++ scripts.js
@@ -0,0 +1,42 @@
+async function loadTasks() {
+    res = await fetch('get-tasks', {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    jsonRes = await res.json()
+
+    ulEL = document.getElementById("tasks")
+    for (task of jsonRes.tasks) {
+        liEL = document.createElement("li")
+        liEL.textContent = task
+        ulEL.appendChild(liEL)
+    }
+}
+
+async function handleSaveTask() {
+    ulEl = document.getElementById("tasks")
+    inputEl = document.getElementById("task")
+
+    liEl = document.createElement("li")
+    liEl.textContent = inputEl.value
+    ulEl.appendChild(liEl)
+
+    payload = {
+        task: inputEl.value
+    }
+
+    res = await fetch('save-task', {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    
+    jsonRes = await res.json()
+    console.log(jsonRes)
+}
